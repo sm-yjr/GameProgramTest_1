5af4350516663a42d26b821062757bbf264b119d
sm-yjr <76152255+sm-yjr@users.noreply.github.com>
2020-12-18 15:01:01 +0800

    test1

--- GameProgramTest_1/GameProgramTest_1.cpp
+++ GameProgramTest_1/GameProgramTest_1.cpp
@@ -7,6 +7,7 @@
 #define MAX_LOADSTRING 100
 
 // 全局变量: 
+//test
 HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
 WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
